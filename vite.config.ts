7a6b9b6be3918cdd42e1c1376c4f0999e0d6e20b
alfyn719 <zyk1191894890@gmail.com>
2024-09-04 17:18:52 +0800

    scaffold: support decorator

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [['@babel/plugin-proposal-decorators', { version: '2023-11' }]],
+    },
+  })],
 
   css: {
     modules: {
