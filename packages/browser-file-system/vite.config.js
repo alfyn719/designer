72f9bc340c5c5d65c54c0bc7ac4ebce61307c203
张一科 <ykz@YKs-MacBook-Pro.local>
2025-01-08 13:46:31 +0800

    chore: browser tools init

---
+++ packages/browser-file-system/vite.config.js
@@ -0,0 +1,32 @@
+import { writeFileSync } from 'node:fs'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { peerDependencies } from './package.json'
+
+export default defineConfig({
+  plugins: [
+    dts({
+      rollupTypes: true,
+      afterBuild(emittedFiles) {
+        emittedFiles
+          .entries()
+          .forEach(([path, content]) =>
+            writeFileSync(path.replace('.d.ts', '.d.cts'), content),
+          )
+      },
+    }),
+  ],
+
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+    },
+    lib: {
+      entry: ['./src/index.ts'],
+      name: 'browser-file-system',
+      formats: ['es', 'cjs'],
+      fileName: 'index',
+    },
+  },
+})
